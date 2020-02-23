90294368510241ac432b9cc04ada202f90f5bede
Aziz Nasirov <nasirovaziz@yahoo.com>
2020-02-23 22:25:33 +0000

    Automatically backed up by Learn

--- lib/school.rb
+++ lib/school.rb
@@ -13,3 +13,18 @@ class School
     @student_name = name
     @grade = grade
     if @roster.include?(grade) == false
+      @roster[grade] = []
+    end
+    @roster [grade] << name
+  end
+  
+  def grade(number)
+    @roster[number]
+  end
+  
+  def sort
+    @roster.each do |name, grade|
+      @roster[grade] = name.sort
+    end
+  end
+end
