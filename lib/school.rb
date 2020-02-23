58c6a28bb646ab2a2497a2ca0df0eeeacd51742e
Aziz Nasirov <nasirovaziz@yahoo.com>
2020-02-23 22:23:12 +0000

    Automatically backed up by Learn

--- lib/school.rb
+++ lib/school.rb
@@ -1,3 +1,15 @@
 class School
   
-  def initialize(school_)
+  def initialize(school_name)
+    @school_name = school_name
+    @roster = {}
+  end
+  
+  def roster
+    @roster
+  end
+  
+  def add_student(name, grade)
+    @student_name = name
+    @grade = grade
+    if @roster.include?(grade) == false
